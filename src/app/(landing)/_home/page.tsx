5c85539a862b609f1c6f85a5b9a5514f3591c47c
Lucca Gabriel <lucca.santos@wikidados.com.br>
2024-05-03 17:38:57 -0300

    ..

--- src/app/(landing)/_home/page.tsx
+++ src/app/(landing)/_home/page.tsx
@@ -1,17 +1,25 @@
+"use client";
+
+// Next
+import { motion } from "framer-motion";
 // Styles
 import styles from "./styles.module.css";
 
 export default function Home() {
   return (
     <div className={`${styles.container} text-white`}>
-      <div>
-        <svg height="40" width="200" xmlns="http://www.w3.org/2000/svg">
-          <text x="5" y="30" fill="none" stroke="red" font-size="35">
-            I love SVG!
-          </text>
-        </svg>
+      <div className="flex flex-col">
+        <motion.span animate={{ opacity: 1, transition: { duration: 1, delay: 0 } }} className="opacity-0 text-9xl font-bold">
+          Hi,
+        </motion.span>
+        <motion.span animate={{ opacity: 1, transition: { duration: 1, delay: 0.2 } }} className="opacity-0 text-9xl font-bold">
+          {"I'm Lucca"}
+        </motion.span>
+        <motion.span animate={{ opacity: 1, transition: { duration: 1, delay: 0.4 } }} className="opacity-0 text-9xl font-bold">
+          Software Developer
+        </motion.span>
       </div>
-      <div>O que eu faço</div>
+      <div className="mt-[3rem]">Computer Engineer - Full-Stack Developer</div>
     </div>
   );
 }
